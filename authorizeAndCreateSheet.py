bb1c90e69992a7228e79fdf1ee0331b3a5c66d02
karissa-xu <39630690+karissa-xu@users.noreply.github.com>
2018-07-17 15:22:55 -0700

    Update authorizeAndCreateSheet.py

--- authorizeAndCreateSheet.py
+++ authorizeAndCreateSheet.py
@@ -26,7 +26,7 @@ coverSheet = sh.add_worksheet('coverSheet', rows=100, cols=10, src_tuple=[templa
 coverSheet.update_cell("B5", time)
 #copy parameter choices
 paraChID = template.worksheet_by_title('parameter choices').id
-sh.add_worksheet('coverSheet', rows=100, cols=10, src_tuple=[templateID,paraChID], index=2)
+sh.add_worksheet('parameter choices', rows=100, cols=10, src_tuple=[templateID,paraChID], index=2)
 print ("fill out cover sheet")
 # delete blank sheet1
 sheet1 = sh.sheet1
